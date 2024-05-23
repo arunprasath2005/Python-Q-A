8aba967a4a408957be6dfeb85205b3a7c68413b4
arunprasath2005 <157440404+arunprasath2005@users.noreply.github.com>
2024-05-23 17:15:46 +0530

    Update Number is Positive or Negative.py

--- Number is Positive or Negative.py
+++ Number is Positive or Negative.py
@@ -1,7 +1,9 @@
 ''' Python Program to check if a Number Is Positive Or Negative '''
 
-n = int(input('Enter the number : '))
-if n % 2 == 0:
-    print('{} is a Positive number'.format(n))
+n = int(input('Enter an number : '))
+if n == 0 :
+    print('Zero')
+elif n>0:
+    print('{} is positive number'.format(n))
 else:
-    print('{} is a negative number'.format(n))
+    print('{} is negative number'.format(n))
