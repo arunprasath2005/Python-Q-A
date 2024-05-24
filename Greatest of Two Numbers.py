a2cb6d2263605ad60ac873baa64060fcb32beb90
arunprasath2005 <157440404+arunprasath2005@users.noreply.github.com>
2024-05-24 08:26:05 +0530

    Create Greatest of Two Numbers.py

---
+++ Greatest of Two Numbers.py
@@ -0,0 +1,9 @@
+''' Find the Greatest of Two Numbers in Python '''
+
+n1 = int(input('Enter the number 1: '))
+n2 = int(input('Enter the number 2: '))
+
+if n1>n2 :
+    print('{} is greater than {}'.format(n1,n2))
+else:
+    print('{} is greater than {}'.format(n2,n1))
